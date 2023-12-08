9722fd58cb7f12452a6464cfd00e71cecb516780
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-12-08 18:23:35 +0530

    inital commit

--- config/site.ts
+++ config/site.ts
@@ -8,62 +8,54 @@ export const siteConfig = {
 			label: "Home",
 			href: "/",
 		},
-    {
-      label: "Docs",
-      href: "/docs",
-    },
-    {
-      label: "Pricing",
-      href: "/pricing",
-    },
-    {
-      label: "Blog",
-      href: "/blog",
-    },
-    {
-      label: "About",
-      href: "/about",
-    }
+		{
+			label: "Create",
+			href: "/create",
+		},
+		// {
+		// 	label: "Pricing",
+		// 	href: "/pricing",
+		// },
+		// {
+		// 	label: "Blog",
+		// 	href: "/blog",
+		// },
+		// {
+		// 	label: "About",
+		// 	href: "/about",
+		// }
 	],
 	navMenuItems: [
 		{
-			label: "Profile",
-			href: "/profile",
+			label: "Home",
+			href: "/",
 		},
 		{
-			label: "Dashboard",
-			href: "/dashboard",
+			label: "Create",
+			href: "/create",
 		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
+		// {
+		// 	label: "Projects",
+		// 	href: "/projects",
+		// },
+		// {
+		// 	label: "Team",
+		// 	href: "/team",
+		// },
+		// {
+		// 	label: "Calendar",
+		// 	href: "/calendar",
+		// },
+		// {
+		// 	label: "Settings",
+		// 	href: "/settings",
+		// },
 	],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
-		twitter: "https://twitter.com/getnextui",
-		docs: "https://nextui-docs-v2.vercel.app",
-		discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev"
+		github: "https://github.com/Code-Decoders",
+		twitter: "https://twitter.com/CodeDecoders",
+		// docs: "https://nextui-docs-v2.vercel.app",
+		discord: "https://discord.gg/PQVWWpk6",
+		// sponsor: "https://patreon.com/jrgarciadev"
 	},
 };
