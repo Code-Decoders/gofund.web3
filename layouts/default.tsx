55b1358c288387f46a5f31467f44749a7480b000
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-12-08 19:03:55 +0530

    updates

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -1,12 +1,15 @@
 import { Navbar } from "@/components/navbar";
 import { Link } from "@nextui-org/link";
 import { Head } from "./head";
+import { useTheme } from "next-themes";
 
 export default function DefaultLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
+	const {theme, setTheme} = useTheme()
+	setTheme('dark')
 	return (
 		<div className="relative flex flex-col h-screen">
 			<Head />
