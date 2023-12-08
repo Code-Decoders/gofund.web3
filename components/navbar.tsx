f94887544617666df78a46a9f88cb3d9bd5ca4af
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-12-08 18:49:53 +0530

    icon added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -27,36 +27,13 @@ import {
   SearchIcon,
 } from "@/components/icons";
 
-import { Logo } from "@/components/icons";
-
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  );
-
   return (
-    <NextUINavbar maxWidth="xl" position="sticky" className="h-24">
+    <NextUINavbar maxWidth="xl" position="sticky" className="h-28">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo className="h-20 w-20" />
+            <img src={"./next.svg"} className="h-16 w-16 p-2 object-contain" />
             <p className="font-bold text-3xl">GoFund.Web3</p>
           </NextLink>
         </NavbarBrand>
@@ -92,11 +69,11 @@ export const Navbar = () => {
           <Link isExternal href={siteConfig.links.github}>
             <GithubIcon className="text-default-500" />
           </Link>
-          <ThemeSwitch />
+          {/* <ThemeSwitch /> */}
         </NavbarItem>
       </NavbarContent>
       <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <ThemeSwitch />
+        {/* <ThemeSwitch /> */}
         <NavbarMenuToggle />
       </NavbarContent>
 
