8fdc3042c32f6b36515ce0581a41d717a0d7c1e7
kunal528 <64953335+kunal528@users.noreply.github.com>
2023-12-10 07:21:21 +0530

    get all added in the web3

--- components/cards/nft-card.js
+++ components/cards/nft-card.js
@@ -6,18 +6,11 @@ const CCOCard = ({nft}) => {
     const router = useRouter()
     return (
         <Card className="col-span-12 sm:col-span-4 h-[300px]">
-            <CardHeader className="absolute z-10 flex !items-start justify-between bg-[#00000045]">
-                <div className='flex-col !items-start'>
-                <p className="text-tiny text-white/60 uppercase font-bold">{nft.title}</p>
-                <h4 className="text-white font-medium text-large">{nft.subtitle}</h4>
-                </div>
-                <Chip radius="full" size="sm" onClick={() => {router.push("/cco/1")}}>{nft.price}</Chip>
-            </CardHeader>
             <Image
                 removeWrapper
                 alt="Card background"
                 className="z-0 w-full h-full object-cover "
-                src="https://analyticsindiamag.com/wp-content/uploads/2020/01/top-10-DS-projects.png"
+                src={nft.image}
             />
         </Card>
     )
